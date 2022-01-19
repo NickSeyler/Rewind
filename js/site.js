b4a0683772fa425979cede9bb90e6cb17b34e974
NickSeyler <nickseyler25@gmail.com>
2022-01-19 13:52:28 -0500

    Init

--- js/site.js
+++ js/site.js
@@ -1,9 +1,31 @@
-//Display message to the user
-function getMessage(){
-    let userMessage = document.getElementById("message").value;
-    Swal.fire(
-        'The App Title',
-        userMessage,
-        'success'
-    );
+//get the string from the user
+function getStr(){
+    let inputStr = document.getElementById("inputStr").value;
+
+    let revStr = reverseAString(inputStr);
+
+    displayData(revStr);
+}
+
+//Input: String
+//Return: String
+//Reverse a String
+function reverseAString(inputStr){
+    let revStr = "";
+
+    for(let i = inputStr.length - 1; i >= 0; i--){
+        revStr += inputStr[i];
+    }
+
+    return revStr;
+}
+
+//Input: String
+//Return: None
+//Display a reversed string
+function displayData(revStr){
+
+    document.getElementById("alertRevStr").innerHTML = `${revStr}`;
+    document.getElementById("alertSuccess").classList.remove("invisible");
+
 }
